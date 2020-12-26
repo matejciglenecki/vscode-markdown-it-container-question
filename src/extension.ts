a902fa92ed3776d10edb1a021a979eb4a0a455c8
matej <matej.ciglenecki@gmail.com>
2020-12-26 01:14:53 +0100

    refix regex

--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,29 @@
 import MarkdownIt = require('markdown-it');
 import * as vscode from 'vscode';
-import {} from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "markdown-it-container-question" is now active!');
   // const userConfig = vscode.workspace.getConfiguration('markdown-it-container-question');
 
   return {
     extendMarkdownIt(md: MarkdownIt) {
       const mdc = require('markdown-it-container');
       const containerName = 'q';
-      const ARG_REG = /^${containerName}\s+(.*)$/;
+      const ARG_REG = /^q\s*(.*)$/;
 
       md.use(mdc, containerName, {
-        render: (tokens: any, idx: any) => {
-          var m = tokens[idx].info.trim().match(ARG_REG);
+        validate: function (params: any) {
+          return params.trim().match(ARG_REG);
+        },
+
+        render: (tokens: any, idx: number) => {
           if (tokens[idx].nesting === 1) {
-            return (
-              `<details>` +
-              `<summary class="question">` +
-              md.utils.escapeHtml(m[1]) +
-              '</summary>' +
-              `<span class="answer">'` +
-              `\n`
-            );
+            const m = tokens[idx].info.trim().match(ARG_REG);
+            const title = md.utils.escapeHtml(m[1]);
+            return `<details>` + `<summary class="question">` + title + '</summary>' + `<span class="answer">'` + `\n`;
           } else {
-            return '</details>\n';
+            return '</span></details>\n';
           }
         },
       });
-
-      console.log(md.render('::: q click me\n*content*\n:::\n'));
 
       return md;
     },
